0ebd76d00431cab07ab6e6deb0dbd5e6d07119c7
minj <mminjjie@gmail.com>
2020-06-13 11:23:53 +0800

    modified docker-entrypoint.sh

--- mysql/etc/docker-entrypoint.sh
+++ mysql/etc/docker-entrypoint.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/bash
 
+mv /usr/local/mysql8.0/data/3306/auto.cnf /usr/local/mysql8.0/data/3306/auto.cnf.bak
+
 exec /usr/local/bin/mysqld --defaults-file=/usr/local/mysql8.0/etc/my.cnf 
 
--- tomcat/docker-entrypoint.sh
+++ tomcat/docker-entrypoint.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
 
-source /etc/profile
-
 exec /usr/local/tomcat/bin/catalina.sh run
 
